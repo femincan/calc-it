b32a76649b5297136640d53c050beb4c8c7e7b89
Furkan Emin Can <femincanx@gmail.com>
2023-07-25 16:58:30 +0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [1, { allowConstantExport: true }],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }],
+    'react/require-default-props': 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+  },
+};
