71820c1656fb166e0fc4be4a6b9902410b09cc4a
Furkan Emin Can <femincanx@gmail.com>
2023-09-03 20:17:10 +0300

    feat: update Display component to show dynamic values

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,18 +1,18 @@
 import { Flex, Text } from '@mantine/core';
+import { selectCalculation } from '@src/app/calculatorSlice';
+import { useAppSelector } from '@src/app/hooks';
 
-export const Display = () => (
-  <Flex
-    direction='column'
-    sx={{
-      direction: 'rtl',
-    }}
-  >
-    {' '}
-    <Text opacity={0.4} size={28} weight={300}>
-      6,291/5
-    </Text>
-    <Text id='display' size={52} weight={300} sx={{ lineHeight: '120%' }}>
-      1,258.2
-    </Text>
-  </Flex>
-);
+export const Display = () => {
+  const calculation = useAppSelector(selectCalculation);
+
+  return (
+    <Flex direction='column' align='flex-end' sx={{ fontSize: 28 }}>
+      <Text opacity={0.4} size='1em' weight={300}>
+        {calculation.pastValue}
+      </Text>
+      <Text id='display' size='2em' weight={300} sx={{ lineHeight: '120%' }}>
+        {calculation.currentValue}
+      </Text>
+    </Flex>
+  );
+};
