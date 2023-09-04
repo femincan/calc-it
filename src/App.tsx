d691d22b3b6ffe53561ff5fa2b3c5e6dd5cc8d11
Furkan Emin Can <femincanx@gmail.com>
2023-09-04 10:48:20 +0300

    feat: add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
-const App = () => <div>App</div>;
+import { Container, Flex } from '@mantine/core';
+import { Calculator } from './components/Calculator';
+import { Footer } from './components/Footer';
+
+const App = () => (
+  <Container>
+    <Flex
+      direction='column'
+      align='center'
+      justify='flex-start'
+      gap='lg'
+      sx={{ minHeight: '100vh', padding: 24 }}
+    >
+      <Calculator />
+      <Footer />
+    </Flex>
+  </Container>
+);
 
 export default App;
