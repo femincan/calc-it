1c2aa2b8a5fc207ebfd3e31f92b41947300e6c0a
Furkan Emin Can <femincanx@gmail.com>
2023-09-14 22:08:34 +0300

    feat: adjust display font size by calculation length

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,13 +1,33 @@
 import { Flex, Text } from '@mantine/core';
 import { selectCalculation } from '@src/app/calculatorSlice';
 import { useAppSelector } from '@src/app/hooks';
+import {
+  CalculationLengthConfig,
+  calculationLengthPresets,
+} from '@src/constants';
+
+const adjustFontSizeByLength = (targetLength: number) => {
+  const { fontSize } = calculationLengthPresets.find(
+    (preset) => targetLength < preset.maxLength,
+  ) as CalculationLengthConfig;
+
+  return fontSize;
+};
 
 export const Display = () => {
   const calculation = useAppSelector(selectCalculation);
+  const fontSize = adjustFontSizeByLength(calculation.currentValue.length);
 
   return (
-    <Flex direction='column' align='flex-end' sx={{ fontSize: 28 }}>
-      <Text opacity={0.4} size='1em' weight={300}>
+    <Flex direction='column' sx={{ fontSize }}>
+      <Text
+        opacity={0.4}
+        size='1em'
+        weight={300}
+        lh='100%'
+        maw='100%'
+        sx={{ overflowWrap: 'break-word' }}
+      >
         {calculation.pastValue}
       </Text>
       <Text id='display' size='2em' weight={300} sx={{ lineHeight: '120%' }}>
